fc8b36af75192d9b3757b78134f03fd99b6fb9c3
goliatone <burgosemi@gmail.com>
2016-03-30 14:21:59 -0400

    update controller

--- src/config/amqp.js
+++ src/config/amqp.js
@@ -8,6 +8,6 @@ module.exports = {
     channel: 'webhook.github.*',
     exchange: process.env.NODE_AMQP_EXCHANGE || ('wework.' + process.env.NODE_ENV),
     client: {
-        url: 'amqp://nxhoolpl:AMfZKA7AvgsdAWPn-NyEDjlAiXo2-tFO@owl.rmq.cloudamqp.com/nxhoolpl'
+        url: process.env.NODE_AMQP_EXCHANGE
     }
 };
