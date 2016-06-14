eb5d0e6d8eb9b8658db14481ffd4f86f5336511c
goliatone <burgosemi@gmail.com>
2016-06-14 17:53:17 -0400

    handlers: github use config object

--- src/handlers/github.js
+++ src/handlers/github.js
@@ -1,12 +1,18 @@
 'use strict';
 
-var webhookHandler = require('express-github-webhook')({ path: '/github-hook', secret: 'secret' });
+var DEFAULTS = { path:
+    '/github-hook', secret: 'secret'
+};
 
-module.exports = function(app){
+module.exports = function(app, config){
+    config = extend({}, DEFAULTS, config);
+
+    var webhookHandler = require('express-github-webhook')(config);
+
     app.use(webhookHandler);
 
     webhookHandler.on('*', function (event, repo, data) {
-        console.log('event handler', event, repo/*, data*/);
+        console.log('github event handler', event, repo/*, data*/);
         app.emit('webhook.github.*', {
             event: event,
             repo: repo,
