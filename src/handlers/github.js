72527a0f9056f6b5958ab300e3daaec0800092b9
goliatone <burgosemi@gmail.com>
2016-06-14 18:08:18 -0400

    handlers: github handle request errors

--- src/handlers/github.js
+++ src/handlers/github.js
@@ -1,4 +1,5 @@
 'use strict';
+var extend = require('gextend');
 
 var DEFAULTS = { path:
     '/github-hook', secret: 'secret'
@@ -6,10 +7,16 @@ var DEFAULTS = { path:
 
 module.exports = function(app, config){
     config = extend({}, DEFAULTS, config);
+    console.log('config github', JSON.stringify(config, null, 4));
 
     var webhookHandler = require('express-github-webhook')(config);
 
     app.use(webhookHandler);
+
+    webhookHandler.on('error', function(err, req, res){
+        console.log('webhookHandler ERROR', err.message);
+        console.log('BODY:', req.body);
+    });
 
     webhookHandler.on('*', function (event, repo, data) {
         console.log('github event handler', event, repo/*, data*/);
