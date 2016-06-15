d6056a6b54e66621f69c27ea5502ba82fd21719d
goliatone <burgosemi@gmail.com>
2016-06-15 11:29:07 -0400

    handlers: github uses DEFAULTS, fix output

--- src/handlers/github.js
+++ src/handlers/github.js
@@ -1,29 +1,31 @@
 'use strict';
 var extend = require('gextend');
 
-var DEFAULTS = { path:
-    '/github-hook', secret: 'secret'
+var DEFAULTS = {
+    path: '/github-hook',
+    secret: 'secret'
 };
 
 module.exports = function(app, config){
     config = extend({}, DEFAULTS, config);
-    console.log('config github', JSON.stringify(config, null, 4));
+    console.log('- config github', JSON.stringify(config, null, 4));
 
     var webhookHandler = require('express-github-webhook')(config);
 
-    app.use(webhookHandler);
-
     webhookHandler.on('error', function(err, req, res){
-        console.log('webhookHandler ERROR', err.message);
+        console.log('webhookHandler github ERROR', err.message);
         console.log('BODY:', req.body);
     });
 
     webhookHandler.on('*', function (event, repo, data) {
-        console.log('github event handler', event, repo/*, data*/);
+        console.log('webhookHandler github event handler', event, repo/*, data*/);
         app.emit('webhook.github.*', {
-            event: event,
             repo: repo,
-            data: data
+            data: data,
+            event: event,
         });
     });
+
+    console.log('- register github webhook handler');
+    app.use(webhookHandler);
 };
