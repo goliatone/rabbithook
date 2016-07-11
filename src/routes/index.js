c8de57024116bccc67a45378776c920476774c49
goliatone <burgosemi@gmail.com>
2016-07-11 14:55:57 -0400

    routes: adding a test payload route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,25 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'RabbitHook' });
+});
+
+router.get('/health', function(req, res, next){
+    res.send({status: true, timestamp: Date.now()});
+});
+
+router.post('/test/payload', function(req, res){
+    console.log('-------------------------------');
+    console.log('TESTING PAYLOAD');
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.headers);
+    console.log('-------------------------------');
+    res.send(200);
 });
 
 module.exports = function(app, config){
